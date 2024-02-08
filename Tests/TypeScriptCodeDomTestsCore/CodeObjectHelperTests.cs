c8dc0d6a5da19bdbb91052bc42c8c1ec0045b340
zijianhuang <zijianhuang2000@gmail.com>
2024-02-09 09:55:36 +1000

    refactoring and more tests

---
+++ Tests/TypeScriptCodeDomTestsCore/CodeObjectHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Fonlow.TypeScriptCodeDom;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace TypeScriptCodeDomTestsCore
+{
+	public class CodeObjectHelperTests
+	{
+		[Fact]
+		public void TestDateTimeIsSimpleType()
+		{
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(DateTime)));
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(DateTime?)));
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(Nullable<DateTime>)));
+		}
+
+		[Fact]
+		public void TestStringIsSimpleType()
+		{
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(string)));
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(System.String)));
+		}
+
+		[Fact]
+		public void TestEnumIsSimpleType()
+		{
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(DemoWebApi.DemoData.AddressType)));
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(DemoWebApi.DemoData.AddressType?)));
+			Assert.True(CodeObjectHelper.IsSimpleType(typeof(Nullable<DemoWebApi.DemoData.AddressType>)));
+		}
+
+
+
+	}
+}
---
+++ Tests/TypeScriptCodeDomTestsCore/TypeMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using Fonlow.TypeScriptCodeDom;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace TypeScriptCodeDomTestsCore
+{
+	public class TypeMapperTests
+	{
+		[Fact]
+		public void TestIsSimpleSystemType()
+		{
+			Assert.True(TypeMapper.IsSimpleSystemType(typeof(DateTime)));
+		}
+
+		[Fact]
+		public void TestIsSimpleSystemTypeWithTypeFullName()
+		{
+			Assert.True(TypeMapper.IsSimpleSystemType("System.DateTime"));
+		}
+	}
+}

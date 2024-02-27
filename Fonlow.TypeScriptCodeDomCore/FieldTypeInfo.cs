1a80237a8664d8a247fae117e0b515d87401f2dd
zijianhuang <zijianhuang2000@gmail.com>
2024-02-28 07:20:49 +1000

    FieldTypeInfo to store Type rather than full type name

--- Fonlow.TypeScriptCodeDomCore/FieldTypeInfo.cs
+++ Fonlow.TypeScriptCodeDomCore/FieldTypeInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace Fonlow.TypeScriptCodeDom
+﻿using System;
+
+namespace Fonlow.TypeScriptCodeDom
 {
 	/// <summary>
 	/// Used in UserData for passing type info of a field to a code generator.
@@ -6,7 +8,12 @@
 	public class FieldTypeInfo
 	{
 		public bool IsComplex { get; set; }
+
 		public bool IsArray { get; set; }
-		public string TypeFullName { get; set; }
+		//public string TypeFullName { get; set; }
+		/// <summary>
+		/// Optional store Clr type for better mapping in code generators
+		/// </summary>
+		public Type ClrType { get; set; }
 	}
 }
